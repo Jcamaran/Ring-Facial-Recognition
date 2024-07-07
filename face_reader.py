4e15068dcccb9f92b9bafcb4885e0aaa9c0de264
Joaquin C <128087254+Jcamaran@users.noreply.github.com>
2024-07-07 14:14:09 -0400

    Create face_reader.py

---
+++ face_reader.py
@@ -0,0 +1,110 @@
+import cv2
+import os
+import face_recognition
+import math
+import numpy as np
+import time
+
+def face_confidence(face_distance, face_match_threshold=0.6):
+    range = (1.0 - face_match_threshold)
+    linear_val = (1.0 - face_distance) / (range * 2.0)
+
+    if face_distance > face_match_threshold:
+        return str(round(linear_val * 100, 2)) + '%'
+    else:
+        value = (linear_val + ((1.0 - linear_val) * math.pow((linear_val - 0.5) * 2, 0.2))) * 100
+        return str(round(value, 2)) + '%'
+
+
+class FaceRecognition:
+    def __init__(self):
+        self.face_locations = []
+        self.face_encodings = []
+        self.face_names = []
+        self.known_face_names = []
+        self.known_face_encodings = []
+        self.process_current_frame = True
+
+    def encode_faces(self):
+        for image in os.listdir('faces'):
+            face_image = face_recognition.load_image_file(f"faces/{image}")
+            face_encoding = face_recognition.face_encodings(face_image)[0]
+
+            self.known_face_encodings.append(face_encoding)
+            self.known_face_names.append(image)
+        print(self.known_face_names)
+
+    def run_recognition(self):
+        door_video_file = "C:\\Users\\camar\\OneDrive\\Documents\\Joa Projects\\facial_recognition\\last_clip\\Front_Door_last_clip.mp4"
+
+        cap = cv2.VideoCapture(door_video_file)
+
+        if not cap.isOpened():
+            print("Error opening video mp4 file")
+            return
+
+        frame_count = 0
+        start_time = time.time()
+        duration = 30  # Duration in seconds
+
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                print("Reached end of video or error occurred")
+                break
+
+            frame_count += 1
+            print(f"Processing frame {frame_count}")
+
+            if self.process_current_frame:
+                small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+                rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+
+                self.face_locations = face_recognition.face_locations(rgb_small_frame)
+                self.face_encodings = face_recognition.face_encodings(rgb_small_frame, self.face_locations)
+
+                self.face_names = []
+                for face_encoding in self.face_encodings:
+                    matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+                    name = 'Unknown'
+                    confidence = 'Unknown'
+
+                    face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+                    best_match_index = np.argmin(face_distances)
+
+                    if matches[best_match_index]:
+                        name = self.known_face_names[best_match_index]
+                        confidence = face_confidence(face_distances[best_match_index])
+
+                    self.face_names.append(f'{name} ({confidence})')
+
+            self.process_current_frame = not self.process_current_frame
+
+            for (top, right, bottom, left), name in zip(self.face_locations, self.face_names):
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
+
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+                cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), -1)
+                cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 255, 255), 1)
+
+            cv2.imshow('Face Recognition', frame)
+
+            if cv2.waitKey(1)== ord('q'):
+                break
+
+            # Check if 30 seconds have passed
+            if time.time() - start_time > duration:
+                print("30 seconds have passed")
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+        print("Finished processing video")
+
+if __name__ == '__main__':
+    fr = FaceRecognition()
+    fr.encode_faces()
+    fr.run_recognition()
